16704e870b1fcfc7d808020339df93d27f6ebc5b
Tatsumi_Baba <tatsumi1680@icloud.com>
2026-04-21 19:21:17 +0900

    add:404ページの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  private
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: :not_found, layout: false
+  end
 end
